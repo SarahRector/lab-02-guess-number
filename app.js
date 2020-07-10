55388961715672e91c3ca022cd3e72b7d1ac8463
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-07-09 17:54:01 -0700

    changed css, added reset button andgame reset logic, disabled submit button at game end

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const myButton = document.getElementById('submit-button');
 const triesLeft = document.getElementById('tries');
 const guessRange = document.getElementById('high-or-low');
 const winLose = document.getElementById('win-or-lose');
-
+const resetButton = document.getElementById('reset-button');
 // initialize state
 // initialize random number generator
 const correctNumber = Math.ceil(Math.random() * 20);
@@ -25,12 +25,24 @@ myButton.addEventListener('click', () => {
     if (userGuess === 0) {
         guessRange.textContent = 'Yay! You got it!';
         winLose.textContent = 'Winner, winner, lobster dinner!';
+        myButton.disabled = true;
     } else if (userGuess === -1) {
         guessRange.textContent = 'Your guess is too low.';
     } else if (userGuess === 1) {
         guessRange.textContent = 'Your guess is too high.';
-    } if (guessLeft < 0) {
+    } 
+    if (guessLeft === 0) {
         winLose.textContent = 'Lost that Lobster, Loser!';
+        myButton.disabled = true;
     }
-    
+
+});
+
+resetButton.addEventListener('click', () => {
+    guessLeft = 4;
+    winLose.textContent = '';
+    guessRange.textContent = '';
+    triesLeft.textContent = guessLeft + ' tries remaining';
+    myButton.disabled = false;
+    document.getElementById('input').value = '';
 });
